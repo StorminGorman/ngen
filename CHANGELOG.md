0ab85c15da942681bea01e7268e6434ed9ea2671
Brian Gorman <brian@gorbotics.com>
2019-11-16 19:55:57 -0600

    hardcoded the templates so they can come with executable when `pub global activate`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,12 @@
+## 0.0.1+2
+
+- Put the templates in as multiline strings so it comes along with the executable
+
+
+## 0.0.1+1
+
+- Put the template directory in the bin folder 
+
 ## 0.0.1
 
 - Initial version
--- lib/generator/component_generator.dart
+++ lib/generator/component_generator.dart
@@ -11,18 +11,13 @@ class ComponentGenerator {
   void generate() async {
     final directory = new Directory(name);
     await directory.create();
-    final scriptFile = new File(Platform.script.toFilePath());
-    final packagePath = scriptFile.parent.parent.path;
-    final scss = new File("$packagePath/lib/templates/component.scss-template");
-    final dart = new File("$packagePath/lib/templates/component.dart-template");
-    final html = new File("$packagePath/lib/templates/component.html-template");
-    createFile(directory, scss, "scss");
-    createFile(directory, dart, "dart");
-    createFile(directory, html, "html");
+    createFile(directory, SCSS_TEMPLATE, "scss");
+    createFile(directory, DART_TEMPLATE, "dart");
+    createFile(directory, HTML_TEMPLATE, "html");
   }
 
-  void createFile(final Directory directory, final File template, final String type) {
-    var contents = template.readAsStringSync();
+  void createFile(final Directory directory, final String template, final String type) {
+    var contents = template;
     contents = contents.replaceAll("\$className", builder.className());
     contents = contents.replaceAll("\$tagName", builder.tagName());
     contents = contents.replaceAll("\$fileName", builder.fileName());
@@ -32,3 +27,34 @@ class ComponentGenerator {
     newFile.writeAsStringSync(contents);
   }
 }
+
+const DART_TEMPLATE = """
+import 'package:angular/angular.dart';
+
+/// [\$className] 
+///
+/// Example Usage:
+/// ```html
+/// <\$tagName></\$tagName>
+/// ```
+///
+@Component(
+  selector: '\$tagName',
+  styleUrls: ['\$fileName.css'],
+  templateUrl: '\$fileName.html',
+  directives: [],
+)
+class \$className {
+
+}
+""";
+
+const HTML_TEMPLATE = """
+<h1>\$className lives!</h1>
+""";
+
+const SCSS_TEMPLATE = """
+:host {
+    /* This styles the enclosing div */
+}
+""";
